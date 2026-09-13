12d18c7fedf3873089d94cb4a5ef52d7aa8cbe2e
Aditya Dasgupta <adityadasguptadg@gmail.com>
2026-09-13 16:14:37 +0530

    version check

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,1 @@
-public class HelloWorld {
-   public static void main(String[] args) {
-       System.out.println("Hello Sevfvsxy, World! Take me home");
-       System.out.println("V3");
-   }
-}
+Bring me sexy thanos
